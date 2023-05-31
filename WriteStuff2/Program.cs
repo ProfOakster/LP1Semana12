af618dec1b5c170bff0a8a7695a310615599b3ea
TomasCarvalho <2606tom@gmail.com>
2023-05-31 23:54:50 +0100

    Create project WriteStuff2

---
+++ WriteStuff2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WriteStuff2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            string input;
+            Queue<string> inputs = new Queue<string>();
+
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input != string.Empty)
+                    inputs.Enqueue(input);
+                else
+                    break;
+            }
+
+            File.WriteAllLines(path, inputs);
+        }
+    }
+}
