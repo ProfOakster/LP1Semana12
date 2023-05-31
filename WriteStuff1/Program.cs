02c63a65c22a16110e64e9ad38a03f8fd8830b0d
TomasCarvalho <2606tom@gmail.com>
2023-05-31 23:39:03 +0100

    Create project WriteStuff1

---
+++ WriteStuff1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WriteStuff1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            do
+            {
+                
+            } while (string.Empty);
+        }
+    }
+}
