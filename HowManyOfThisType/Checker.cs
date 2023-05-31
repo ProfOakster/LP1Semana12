eb1b52917b33fde8fdba8e36ded9057f64bfe1b0
TomasCarvalho <2606tom@gmail.com>
2023-05-31 16:18:58 +0100

    Create project HowManyOfThisType

---
+++ HowManyOfThisType/Checker.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HowManyOfThisType
+{
+    public static class Checker
+    {
+        public static int HowManyOfType<T>(IEnumerable<object> items)
+        {
+            
+        }
+    }
+}
